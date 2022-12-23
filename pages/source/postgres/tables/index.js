7bf34170afd2b47ce2ec9dea3e7cd6c5b0a77726
Joaquin Colacci <joaquincolacci@gmail.com>
2022-12-23 20:14:01 +0100

    Init

--- pages/source/postgres/tables/index.js
+++ pages/source/postgres/tables/index.js
@@ -44,7 +44,7 @@ export default function Topics() {
                         className="w-fit p-10 mx-auto mt-10 inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         onClick={onNext}
                     >
-                        Build
+                        Replicate
                     </button>
                 </fieldset>
             </div>
